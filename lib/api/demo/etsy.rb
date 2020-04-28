afe21a2e29a0b590c12ad339a44f52c17a375c5d
Gingertonic <bethmschofield@gmail.com>
2020-04-28 14:15:18 +0100

    init

---
+++ lib/api/demo/etsy.rb
@@ -0,0 +1,10 @@
+require "api/demo/etsy/version"
+
+module Api
+  module Demo
+    module Etsy
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/api/demo/etsy/version.rb
@@ -0,0 +1,7 @@
+module Api
+  module Demo
+    module Etsy
+      VERSION = "0.1.0"
+    end
+  end
+end
